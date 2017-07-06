c4f0e8f1b808bb7d65c11453460356353d9ebeff
Jobst Ziebell <Jobst.Ziebell@tu-dresden.de>
2017-07-06 17:52:10 +0200

    Implemented multiplication of polynomials and added some more tests.

--- polynomials.m
+++ polynomials.m
@@ -1,35 +1,67 @@
 (* ::Package:: *)
 
 BeginPackage["polynomials`"];
-Clear[polynomial]
-Clear[polynomialPlus]
+Clear[polynomial];
+Clear[constantTerm];
+Clear[polynomialPlus];
+Clear[polynomialTimes];
 
 polynomial::usage = "Represents a polynomial. polynomial[] expects a Sequence of Lists of \
 the form {coefficient,form} where form is a List of tuples \
 {{name1_Symbol,power1_Integer}, ...} where all powers have to be at least one. A \
-constant term may be represented as {constant}. Every power has to be unique."
+constant term may be represented as {constant}. Every power has to be unique.";
 SetAttributes[polynomial,Orderless];
 
-constantTerm[polynomial[___,{constant_},___]] := constant 
+constantTerm::usage = "Gives the constant term of a p_polynomial.";
+polynomialPlus::usage = "Adds two polynomials.";
+polynomialTimes::usage = "Multiplies two polynomials.";
+
+constantTerm[polynomial[{constant_},___]] := constant 
 constantTerm[polynomial[___]] := 0 
  
 polynomialPlus[polynomial[],p2_polynomial] := p2 
-polynomialPlus[polynomial[{c1_},tail___],polynomial[front___,{c2_},back___]] := 
+polynomialPlus[polynomial[{c1_},tail1___],polynomial[{c2_},tail2___]] := 
   polynomial[{c1+c2},
               Sequence @@ polynomialPlus[   
-                polynomial[tail], 
-                polynomial[front,back]]]  
-polynomialPlus[polynomial[{c1_,form_List},tail___],
-               polynomial[front___,{c2_,form_List},back___]] :=  
-  polynomial[{c1+c2,form},
+                polynomial[tail1], 
+                polynomial[tail2]]]
+polynomialPlus[polynomial[{c1_,form1_List},tail1___],
+               polynomial[{c2_,form2_List},tail2___]] :=  
+  polynomial[{c1+c2,form1},
               Sequence @@ polynomialPlus[ 
-                polynomial[tail], 
-                polynomial[front,back]]]   
+                polynomial[tail1], 
+                polynomial[tail2]]] /; equalForms[form1,form2]
 polynomialPlus[polynomial[term_List,tail___],p2_polynomial] := 
   polynomial[term,Sequence @@ polynomialPlus[polynomial[tail],p2]]
 
+polynomialTimes[polynomial[],p2_polynomial] := polynomial[]
+polynomialTimes[polynomial[{c1_},tail___],p2_polynomial] :=
+  polynomialPlus[p2 /. {c2_,form___} :> {c1*c2,form},
+                 polynomialTimes[polynomial[tail],p2]]
+polynomialTimes[polynomial[{c1_,form_List},tail___],
+                p2_polynomial] :=
+  polynomialPlus[multiplyWithForm[polynomialTimes[polynomial[{c1}],p2],form],
+                 polynomialTimes[polynomial[tail],p2]]
+
 Begin["private`"]; 
- 
+Clear[equalForms];
+Clear[multiplyTermWithForm];
+Clear[multiplyForms];
+
+equalForms[form1_List,form2_List] := (Sort[form1] == Sort[form2])
+
+multiplyWithForm[polynomial[terms__],form_List] :=
+  polynomial @@ (multiplyTermWithForm[#,form] & /@ {terms})
+
+multiplyTermWithForm[{c1_},form_List] := {c1, form}
+multiplyTermWithForm[{c1_,form1_List},form2_List] := {c1, multiplyForms[form1,form2]}
+
+multiplyForms[{},form_List] := form
+multiplyForms[{{x_,p1_},tail___},{front___,{x_,p2_},back___}] := 
+  Join[multiplyForms[{tail},{front,back}],{{x,p1+p2}}]
+multiplyForms[{{x_,p1_},tail___},form2_List] := 
+  Join[multiplyForms[{tail},form2],{{x,p1}}]
+
 End[] (* "private`" *)
 EndPackage[]
 
--- test.m
+++ test.m
@@ -2,6 +2,20 @@
 
 Needs["betaFunctions`"];
 
+(* Test "polynomials" module *)
+
+polynomialPlus[polynomial[{37}],polynomial[{5}]] == polynomial[{42}]
+polynomialPlus[polynomial[{37,{{g,3},{y,2}}},{2,{{g,1}}}],
+              polynomial[{5},{5,{{g,3},{y,2}}},{3,{{y,2}}}]] == 
+              polynomial[{42,{{g,3},{y,2}}},{2,{{g,1}}},{5},{3,{{y,2}}}]
+
+polynomialTimes[polynomial[{37}],polynomial[{5}]] == polynomial[{37*5}]
+polynomialTimes[polynomial[{37,{{g,3},{y,2}}},{2,{{g,1}}}],
+                polynomial[{5},{3,{{y,2}}}]] ==
+                polynomial[{37*5,{{g,3},{y,2}}},{37*3,{{g,3},{y,4}}},{10,{{g,1}}},{6,{{g,1},{y,2}}}]
+
+(* Test "betaFunctions" module *)
+
 coeff=solve[{betaFunctionEquation[g,polynomial[{1},{2,{{g,2},{y,1}}}]],
              betaFunctionEquation[y,polynomial[{1,{{y,3}}}]]},
             {initialCondition[g,42],
@@ -11,6 +25,9 @@ coeff=solve[{betaFunctionEquation[g,polynomial[{1},{2,{{g,2},{y,1}}}]],
 Table[coeff[[1,2]][k],{k,0,3}] == {42,3529,298200,75603446/3}
 
 Table[coeff[[2,2]][k],{k,0,3}] == {1,1,3/2,5/2}
+
+
+
 
 
 
